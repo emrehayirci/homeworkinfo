46c66a484e2e5613bfe121aa541a7498d485a459
Emre Hayırcı <emrehayirci@gmail.com>
2017-05-28 21:29:31 +0300

    added default tmeplates of e-branch

--- e_branch/urls.py
+++ e_branch/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    # url(r'^(?P<pk>[0-9]+)/$', views.UserProfile.as_view(), ),
-    url(r'^$', views.settings, name='settings'),
-
+    url(r'^login/$', login, name='login'),
+    url(r'^accounts/', accounts, name='accounts'),
+    url(r'^$register/', register, name='index'),
+    url(r'^$register/', register, name='index'),
+    url(r'^$', index, name='index'),
 ]
--- e_branch/views.py
+++ e_branch/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from accounts import models
 # Create your views here.
+def index (request):
+
+    return render(request, 'e-branch/_authorized.html')
+
+def login (request):
+    return render(request, 'e-branch/login.html')
+
+def register (request):
+    return render(request, 'e-branch/register')
+
+def accounts (request):
+    return render(request, 'e-branch/accounts')
+
+def loans (request):
+    return render(request, 'e-branch/loans')

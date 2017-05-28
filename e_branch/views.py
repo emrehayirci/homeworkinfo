f6d06480b23ec1016a9f7cc1143c3b2a444d5639
Emre Hayırcı <emrehayirci@gmail.com>
2017-05-28 23:51:49 +0300

    added identity_number field

--- e_branch/views.py
+++ e_branch/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from accounts import models
+from accounts.models import *
+from users.models import User
+from django.contrib.auth.decorators import login_required
+
+#from django.contrib.auth import
 # Create your views here.
+@login_required(login_url='login')
 def index (request):
 
     return render(request, 'e-branch/_authorized.html')
@@ -9,10 +14,16 @@ def login (request):
     return render(request, 'e-branch/login.html')
 
 def register (request):
-    return render(request, 'e-branch/register')
+    return render(request, 'e-branch/register.html')
 
+@login_required(login_url='login')
 def accounts (request):
-    return render(request, 'e-branch/accounts')
+    return render(request, 'e-branch/accounts.html')
 
+@login_required(login_url='login')
 def loans (request):
-    return render(request, 'e-branch/loans')
+    return render(request, 'e-branch/loans.html')
+
+@login_required(login_url='login')
+def transactions (request):
+    return render(request, 'e-branch/transactions.html')
